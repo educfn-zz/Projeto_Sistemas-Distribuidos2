0d9926d933ccdc15612b2904e9202161f0ed0930
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2019-10-28 13:37:24 -0300

    V0.2 - sockets em Java - Parte 2
    Reproduzido o código mostrado no video "Aplicação cliente-servidor com sockets em Java - Parte 2"

--- Socket_Cliente-Servidor/src/PacoteServidor/SocketServidor.java
+++ Socket_Cliente-Servidor/src/PacoteServidor/SocketServidor.java
@@ -1,14 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PacoteServidor;
 
-/**
- *
- * @author EduardoCFN
- */
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SocketServidor {
+
+    /**
+     * 1- Criar o servidor de conexões
+     * 2- Esperar o um pedido de conexão - Outro processo
+     * 2.1- Criar uma nova conexão;
+     * 3- Criar streams de entrada e saída;
+     * 4- Tratar a conversação entre cliente e servidor (tratar protoclo);
+     * 4.1- Fechar socket de comunicação entre servidor/cliente
+     * 4.2- Fechar streams de entrada e saída
+     * 
+     * 5- Voltar para o passo 2, até que finalize o programa;
+     * 6- Fechar serverSocket
+     */
+    
+    private ServerSocket serverSocket;
+    
+    private void criarServerSocket(int porta) throws IOException 
+    {
+        serverSocket = new ServerSocket();
+    }
+    
+    private Socket esperaConexao() throws IOException
+    {
+       Socket socket =  serverSocket.accept();
+       return socket;
+    }
+    
+    private void fechaSocket(Socket s) throws IOException
+    {
+        s.close();
+    }
+    
+    private void trataConexao(Socket socket) throws IOException
+    {
+        //Local onde será aplicado o protocolo da aplicação
+        /*
+        * 3- Criar streams de entrada e saída;
+        * 4- Tratar a conversação entre cliente e servidor (tratar protoclo);
+        *
+        * 1(Um) Socket para cada cliente de Servidor.
+        */
+        
+        try 
+        {
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+        /*
+        * O 'ObjectInputStream' serve como um 'decorator' do InputStream, assim
+        *como no caso do OutputStream; para poder escrever(write) e ler (read) 
+        *o objeto de forma mais fácil, não necessitando assim ficar realizado 
+        * sucessivas converssões de Objeto em bytes e vice-versa. O decorator 
+        * ira fazer a comunicação com a Stream.
+        */
+            
+        String msg = input.readUTF();
+        System.out.println("Mensagem recebida...");
+        output.writeUTF("Hello World!");
+        
+        
+        //4.2- Fechar streams de entrada e saída
+        input.close();
+        output.close();
+        
+        } 
+        catch (IOException e) 
+        {
+            //Area para o tratamento de falhas na comunicação 
+            //com 1(um) cliente(Client).
+        }
+        finally
+        {
+            //4.1- Fechar socket de comunicação entre servidor/cliente
+            fechaSocket(socket);
+        }
+        
+     
+        
+    }
+    
+    public static void main(String[] args) 
+    {
+        try
+        {
+            SocketServidor server = new SocketServidor();
+            
+            //Para teste
+            System.out.println("Aguando conexão...");
+            
+            server.criarServerSocket(5555);                    
+            Socket socket = server.esperaConexao();
+            
+            //Para teste
+            System.out.println("Cliente conectado");
+            
+            server.trataConexao(socket);
+            
+             //Para teste
+            System.out.println("Cliente finalizado.");
+        }
+        catch (IOException e)
+        {
+            //tratar exceção
+        }
+    }
     
 }
