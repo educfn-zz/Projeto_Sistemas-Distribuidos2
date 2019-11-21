0fbf6726d4781001c01a43379b681805cf07d1ac
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2019-11-21 17:37:49 -0300

    V0.2.4.1 - Erro do pacote Mensagem
    Paocte e .java foram movidos para um só projeto, porém ocorreu o problema de ter 2 main em 1 só projeto.

--- Socket_Cliente_Servidor/src/PacoteCliente/SocketCliente.java
+++ Socket_Cliente_Servidor/src/PacoteCliente/SocketCliente.java
@@ -1,5 +1,7 @@
 package PacoteCliente;
 
+import PacoteServidor.Mensagem;
+import PacoteServidor.Status;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
--- Socket_Servidor/build/classes/PacoteServidor/README.txt
+++
@@ -1,8 +0,0 @@
-README - PscoteServidor
-
-SocketServidor.java
-
-
-Link da Playlist com o código utilizado:
-https://www.youtube.com/playlist?list=PLWZB9ryZXz1Psb8R2TmOgn16xc0ixhTCn
-
